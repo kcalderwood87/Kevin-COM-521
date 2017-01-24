8384e044cd81753adb5e5e0517cc42b59784bafe
kcalderwood87 <kjcalder@uw.edu>
2017-01-24 15:24:22 -0800

    update

--- Week 4/week4.R
+++ Week 4/week4.R
@@ -17,13 +17,13 @@ is.na(COS_Statistics_Top5000_Pages)
 #Create a random subsample and then look at the data
 
 MobileSessionsSample1 <- COS_Statistics_Mobile_Sessions[sample(1:nrow(COS_Statistics_Mobile_Sessions), 50, replace=FALSE),]
-list(MobileSessionsSample1)
+MobileSessionsSample1
 MobileSessionsSample2 <- COS_Statistics_Mobile_Sessions[sample(1:nrow(COS_Statistics_Mobile_Sessions), 50, replace=FALSE),]
-list(MobileSessionsSample2)
+MobileSessionsSample2
 Top5000sample1 <- COS_Statistics_Top5000_Pages[sample(1:nrow(COS_Statistics_Top5000_Pages), 50, replace=FALSE),]
-list(Top5000sample1)
+Top5000sample1
 Top5000sample2 <- COS_Statistics_Top5000_Pages[sample(1:nrow(COS_Statistics_Top5000_Pages), 50, replace=FALSE),]
-list(Top5000sample2)
+Top5000sample2
 
 ###PC3
 
@@ -33,7 +33,7 @@ objects(COS_Statistics_Top5000_Pages)
 tapply(COS_Statistics_Top5000_Pages$Pageviews, COS_Statistics_Top5000_Pages$Month, sum)
 Monthlypageviews = aggregate(COS_Statistics_Top5000_Pages$Pageviews, list(COS_Statistics_Top5000_Pages$Month), sum)
 class(Monthlypageviews)
-list(Monthlypageviews)
+Monthlypageviews
 
 ###PC4
 
@@ -43,14 +43,14 @@ objects(COS_Statistics_Mobile_Sessions)
 tapply(COS_Statistics_Mobile_Sessions$Sessions*COS_Statistics_Mobile_Sessions$PagesPerSession, COS_Statistics_Mobile_Sessions$Month, sum)
 Mobilepageviews = aggregate(COS_Statistics_Mobile_Sessions$Sessions*COS_Statistics_Mobile_Sessions$PagesPerSession, list(COS_Statistics_Mobile_Sessions$Month), sum)
 class(Mobilepageviews)
-list(Mobilepageviews)
+Mobilepageviews
 
 ###PC5
 
 #Merge the two datasets together
 
 Mergedviews = merge(Monthlypageviews, Mobilepageviews, all.x=TRUE, all.y=TRUE, by = "Group.1")
-list(Mergedviews)
+Mergedviews
 is.na(Mergedviews)
 
 ###PC6
@@ -58,20 +58,26 @@ is.na(Mergedviews)
 #Create a new column with the percentage of mobile page views
 
 Mergedviews$Percentage <- (Mergedviews$x.y / Mergedviews$x.x)*100
-list(Mergedviews)
+Mergedviews
 
 #Make a datetime object in R and order the dataframe by date
 
 Mergedviews$Group.1 <- gsub(x=Mergedviews$Group.1, pattern = "12:00:00 AM", replacement = "", fixed = T)
 Mergedviews$Group.1 <- as.Date(Mergedviews$Group.1, "%m/%d/%Y")
-list(Mergedviews$Group.1)
+Mergedviews$Group.1
 class(Mergedviews$Group.1)
 Mergedviews <- Mergedviews[order(as.Date(Mergedviews$Group.1, format="%Y/%m/%d")),]
-list(Mergedviews)
+Mergedviews
+
+#Drop missing values
+
+Mergedviews1 <- Mergedviews[apply(Mergedviews, 1, function (x) {!all(is.na(x))}),]
+Mergedviews1 <- Mergedviews1[complete.cases(Mergedviews1),]
+Mergedviews1
 
 ###PC6 Part 2
 
 #Plot the change over time in mobile views
 
 library(ggplot2)
-ggplot(data=Mergedviews) + geom_point() + aes(x=Group.1, y=Percentage)
+ggplot(data=Mergedviews1) + geom_point() + aes(x=Group.1, y=Percentage)
